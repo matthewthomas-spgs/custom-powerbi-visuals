2330ab9bb7fa4eb64d07daf90140c67ed19e4691
Matthew Thomas <matt.thomas003@outlook.com>
2026-02-24 06:57:53 +1100

    pacakge created along with txt file verbose output with dev suggestions

--- riskMatrix/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++ riskMatrix/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -1,4 +1,4 @@
 import powerbiVisualsApi from "powerbi-visuals-api";
 import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var riskMatrixE6C2180271CC43D3B13A714F2455D1DE_DEBUG: IVisualPlugin;
-export default riskMatrixE6C2180271CC43D3B13A714F2455D1DE_DEBUG;
+declare var riskMatrixE6C2180271CC43D3B13A714F2455D1DE: IVisualPlugin;
+export default riskMatrixE6C2180271CC43D3B13A714F2455D1DE;
